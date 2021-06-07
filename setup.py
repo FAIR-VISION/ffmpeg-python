b5a30ce61230ac86bb6b8a2b244e6c381fb2fc1b
JeanLecigne <j.lecigne@fairvision.fr>
2021-06-07 10:37:32 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 from textwrap import dedent
 
-version = '0.2.0'
-download_url = 'https://github.com/kkroening/ffmpeg-python/archive/v{}.zip'.format(
+version = '0.0.1'
+download_url = 'https://github.com/FAIR-VISION/ffmpeg-python/archive/refs/heads/main.zip'.format(
     version
 )
 
@@ -11,9 +11,7 @@ long_description = dedent(
     ffmpeg-python: Python bindings for FFmpeg
     =========================================
 
-    :Github: https://github.com/kkroening/ffmpeg-python
-    :API Reference: https://kkroening.github.io/ffmpeg-python/
-'''
+    '''
 )
 
 
@@ -61,12 +59,11 @@ setup(
     name='ffmpeg-python',
     packages=['ffmpeg'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-mock'],
     version=version,
-    description='Python bindings for FFmpeg - with complex filtering support',
-    author='Karl Kroening',
-    author_email='karlk@kralnet.us',
-    url='https://github.com/kkroening/ffmpeg-python',
+    description='Python bindings for FFmpeg fv soft',
+    author='extstagiaire1',
+    author_email='support@fairvision.fr',
+    url='https://github.com/FAIR-VISION/ffmpeg-python.git',
     download_url=download_url,
     keywords=keywords,
     long_description=long_description,
@@ -76,7 +73,6 @@ setup(
             'future==0.17.1',
             'numpy==1.16.4',
             'pytest-mock==1.10.4',
-            'pytest==4.6.1',
             'Sphinx==2.1.0',
             'tox==3.12.1',
         ]
