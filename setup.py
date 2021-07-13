2a8833467dc1b7121ba98114d1fdc20c52156600
FairVisionPublic <85477965+FairVisionPublic@users.noreply.github.com>
2021-07-13 11:30:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ keywords = misc_keywords + file_formats
 setup(
     name='ffmpeg-python',
     packages=['ffmpeg', 'ffmpeg.third_part'],
-    include_package_data=True
+    include_package_data=True,
     setup_requires=['pytest-runner'],
     version=version,
     description='Python bindings for FFmpeg fv soft',
